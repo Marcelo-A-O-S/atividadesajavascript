6eded3ff3b6e3934b461e92854259ecd9b6b605d
Marcelo Augusto <77033790+Marcelo-A-O-S@users.noreply.github.com>
2022-10-21 00:04:50 -0300

    commit

---
+++ navegacao.js
@@ -0,0 +1,128 @@
+function Home()
+{
+    var Home = document.getElementById("Home")
+    Home.style.display= "flex";
+
+    var CadastrarS = document.getElementById("CadastrarS")
+    CadastrarS.style.display= "none";
+
+    var VerificarS = document.getElementById("VerificarS")
+    VerificarS.style.display= "none";
+
+    var ListarS = document.getElementById("ListarS")
+    ListarS.style.display= "none";
+
+    var ModificarS = document.getElementById("ModificarS")
+    ModificarS.style.display= "none";
+
+    var FiltrarS = document.getElementById("FiltrarS")
+    FiltrarS.style.display= "none";
+    
+}
+function CadastrarS()
+{
+    var Home = document.getElementById("Home")
+    Home.style.display= "none";
+
+    var CadastrarS = document.getElementById("CadastrarS")
+    CadastrarS.style.display= "grid";
+
+    var VerificarS = document.getElementById("VerificarS")
+    VerificarS.style.display= "none";
+
+    var ListarS = document.getElementById("ListarS")
+    ListarS.style.display= "none";
+
+    var ModificarS = document.getElementById("ModificarS")
+    ModificarS.style.display= "none";
+
+    var FiltrarS = document.getElementById("FiltrarS")
+    FiltrarS.style.display= "none";
+    
+}
+function VerificarS()
+{
+    var Home = document.getElementById("Home")
+    Home.style.display= "none";
+
+    var CadastrarS = document.getElementById("CadastrarS")
+    CadastrarS.style.display= "none";
+
+    var VerificarS = document.getElementById("VerificarS")
+    VerificarS.style.display= "grid";
+
+    var ListarS = document.getElementById("ListarS")
+    ListarS.style.display= "none";
+
+    var ModificarS = document.getElementById("ModificarS")
+    ModificarS.style.display= "none";
+
+    var FiltrarS = document.getElementById("FiltrarS")
+    FiltrarS.style.display= "none";
+    
+}
+function ListarS()
+{
+    var Home = document.getElementById("Home")
+    Home.style.display= "none";
+
+    var CadastrarS = document.getElementById("CadastrarS")
+    CadastrarS.style.display= "none";
+
+    var VerificarS = document.getElementById("VerificarS")
+    VerificarS.style.display= "none";
+
+    var ListarS = document.getElementById("ListarS")
+    ListarS.style.display= "grid";
+
+    var ModificarS = document.getElementById("ModificarS")
+    ModificarS.style.display= "none";
+
+    var FiltrarS = document.getElementById("FiltrarS")
+    FiltrarS.style.display= "none";
+
+    ListarTabela()
+    
+}
+function ModificarS()
+{
+    var Home = document.getElementById("Home")
+    Home.style.display= "none";
+
+    var CadastrarS = document.getElementById("CadastrarS")
+    CadastrarS.style.display= "none";
+
+    var VerificarS = document.getElementById("VerificarS")
+    VerificarS.style.display= "none";
+
+    var ListarS = document.getElementById("ListarS")
+    ListarS.style.display= "none";
+
+    var ModificarS = document.getElementById("ModificarS")
+    ModificarS.style.display= "grid";
+
+    var FiltrarS = document.getElementById("FiltrarS")
+    FiltrarS.style.display= "none";
+    
+}
+function FiltrarS()
+{
+    var Home = document.getElementById("Home")
+    Home.style.display= "none";
+
+    var CadastrarS = document.getElementById("CadastrarS")
+    CadastrarS.style.display= "none";
+
+    var VerificarS = document.getElementById("VerificarS")
+    VerificarS.style.display= "none";
+
+    var ListarS = document.getElementById("ListarS")
+    ListarS.style.display= "none";
+
+    var ModificarS = document.getElementById("ModificarS")
+    ModificarS.style.display= "none";
+
+    var FiltrarS = document.getElementById("FiltrarS")
+    FiltrarS.style.display= "grid";
+    
+}
